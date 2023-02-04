bf344f58192224c29343b0f85c4db3d352352ec7
dollytiwari <dollytiwari393@gmail.com>
2023-02-04 21:30:39 +0530

    just check

---
+++ src/main/webapp/Demo.java
@@ -0,0 +1,14 @@
+package main.webapp;
+
+public class Demo {
+
+	public Demo() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hii welcome");
+
+	}
+
+}
