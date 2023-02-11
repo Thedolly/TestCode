f456f6fd0ec1eb3cfcb73f12fba679d6d10cb200
dollytiwari <dollytiwari393@gmail.com>
2023-02-12 00:10:55 +0530

    accept array and print

--- src/main/webapp/Demo.java
+++ src/main/webapp/Demo.java
@@ -1,13 +1,39 @@
 package main.webapp;
 
+import java.util.Scanner;
+
 public class Demo {
+	
+	
+	
+	
 
 	public Demo() {
-		// TODO Auto-generated constructor stub
+		
 	}
 
 	public static void main(String[] args) {
-		System.out.println("Hii welcome !!!! !");
+		Scanner sc = new Scanner(System.in);
+		
+		
+		
+		System.out.println("Enter array size");
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		System.out.println("Enter array element");
+		int a[][] = new int[n][m];
+		
+		for(int i = 0 ; i<a.length ;i++) {
+			for(int j=0; j<a[i].length; j++) {
+				a[i][j] = sc.nextInt();
+			}
+		}
+		for(int i = 0; i<a.length ; i++) {
+			for(int j=0; j<a[i].length; j++) {
+				System.out.print(a[i][j]+ " ");
+			}
+			System.out.println();
+		}
 
 	}
 
